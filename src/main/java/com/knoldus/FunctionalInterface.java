3eac544f8f067075e95d061d9a5017712eda34fc
Manas-kashyap <manaskashyaptech@gmail.com>
2020-03-04 11:52:45 +0530

    README modified

--- src/main/java/com/knoldus/FunctionalInterface.java
+++
@@ -1,6 +0,0 @@
-package com.knoldus;
-
-@java.lang.FunctionalInterface
-public interface FunctionalInterface {
-    String concatenateStrings(String first, String second, String third, String fourth, String fifth);
-}
